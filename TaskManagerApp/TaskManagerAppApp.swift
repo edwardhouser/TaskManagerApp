70cd7685a95d242f94886ba025d64599f1a008f6
Edward Houser <edward.houser@gmail.com>
2025-12-09 18:26:56 -0800

    Initial Commit

---
+++ TaskManagerApp/TaskManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAppApp.swift
+//  TaskManagerApp
+//
+//  Created by Edward Houser on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
